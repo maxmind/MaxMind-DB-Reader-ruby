a40a8e93a30691060297623ce37b7a9178a3de62
Will Storey <will@summercat.com>
2018-08-15 12:40:20 -0700

    Inherit from a more specific class

--- lib/maxmind/db/errors.rb
+++ lib/maxmind/db/errors.rb
@@ -2,7 +2,7 @@ module MaxMind # :nodoc:
   class DB
     # An InvalidDatabaseError means the {MaxMind
     # DB}[http://maxmind.github.io/MaxMind-DB/] file is corrupt or invalid.
-    class InvalidDatabaseError < Exception
+    class InvalidDatabaseError < RuntimeError
     end
   end
 end
